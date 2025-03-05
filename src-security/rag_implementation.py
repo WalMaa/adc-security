89e2a6aca1e152054e29f72a03f8c84d9cfa5891
VKasurinen <vaino.kasurinen@hotmail.fi>
2025-03-05 20:22:17 +0200

    countermeasure id in rag works

--- src-security/rag_implementation.py
+++ src-security/rag_implementation.py
@@ -3,16 +3,32 @@ from langchain.chains import RetrievalQA
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 from langchain.prompts import PromptTemplate
 from rag_loader import load_rag
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import CSVLoader
+import pandas as pd
+
+
+def preprocess_remediation_table(file_path):
+    df = pd.read_csv(file_path, encoding="utf-8-sig")
+    df.ffill(inplace=True)
+    preprocessed_path = file_path.replace(".csv", "_preprocessed.csv")
+    df.to_csv(preprocessed_path, index=False, encoding="utf-8-sig")
+    return preprocessed_path
+
+
+remediation_table_path = "./sheets/remediation_table.csv"
+preprocessed_remediation_table_path = preprocess_remediation_table(remediation_table_path)
+
 
 files = [
-"./sheets/scenarios_threats.csv",
-"./sheets/scenarios_vulnerability.csv",
+"./sheets/Scenarios_Threats.csv",
+"./sheets/Scenarios_Vulnerability.csv",
 # "./sheets/scenarios_examples.csv",
-"./sheets/remediation_table.csv"
+# "./sheets/remediation_table.csv"
+preprocessed_remediation_table_path
 ]
 
 documents = []
@@ -22,7 +38,9 @@ for file in files:
     documents.extend(loader.load())
 print("Documents loaded.")
 
-model_name = "llama3.1"
+# model_name = "llama3.1"
+
+model_name = "llama3"
 
 # Split the document into chunks
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100, separator="\n")
@@ -55,6 +73,7 @@ llm = ChatOllama(model=model_name, temperature=0.2,
 # Create Retrieval-Augmented Generation (RAG) system
 qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff" , retriever=retriever)
 
-result = qa_chain.invoke("I have flooding in my server room, give a proper threat id, vulnerability id and countermeasure id for this",)
+# result = qa_chain.invoke("I have flooding in my server room, give a proper threat id, vulnerability id and countermeasure id for this",)
+result = qa_chain.invoke("I have system where anyone can access it, give a proper threat id, vulnerability id and countermeasure id for this",)
 print(result)
 
